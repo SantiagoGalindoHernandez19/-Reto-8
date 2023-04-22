c573a1d4f05b696761f0c28aba305de3607f8c0a
SantiagoGalindoHernandez19 <124641609+SantiagoGalindoHernandez19@users.noreply.github.com>
2023-04-21 20:03:55 -0500

    Add files via upload

---
+++ punto 2, reto 8.py
@@ -0,0 +1,8 @@
+
+range(1,1000,2) #Establecemos rango y salto de numeros
+for n in range (1, 1000, 2): # Variable (n) donde guardaremos los valores
+    print(n) #Impresion listado
+
+range(2,1001,2) #Establecemos rango y salto de numeros
+for n in range (2, 1001, 2): # Variable (n) donde guardaremos los valores
+    print(n)
