da4f06815c1c26efa4cff31b2744b52fc244dca4
SantiagoGalindoHernandez19 <124641609+SantiagoGalindoHernandez19@users.noreply.github.com>
2023-04-21 23:38:01 -0500

    Add files via upload

---
+++ Punto 5, reto 8.py
@@ -0,0 +1,9 @@
+
+x = int(input("Ingresar un numero ")) # Solicitamos numero al usuario
+producto = 1 # Determinacion primera variable donde desencadenamos todo
+range(x) # Determinacion del rango
+for n in range(x): # Variable n
+    producto *= 2
+
+print( "2 elevado a " ,x, " da como resultado ", producto)
+
